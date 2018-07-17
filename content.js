33c5fdb7aa72e8d2bfc9297aea6238e3b26896c6
Ankit Gupta <ankit0504@gmail.com>
2018-07-17 19:56:19 -0400

    synonym api

--- content.js
+++ content.js
@@ -1,14 +1,21 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
-      var selected = (window.getSelection().toString()).split(" ")
-      //chrome.runtime.sendMessage({"message": "open_new_tab", "url": selected});
+      var selected = (window.getSelection().toString()).split(" ");
       for (let word of selected) {
-
+        get_synonym(word)
       }
     }
   }
-  function get_synonym(word){
-
-  }
 );
+function get_synonym(word){
+  var syn = require("synonyms");
+  var synArray = syn(word);
+  console.log("SYNARRAY: "+ synArray)
+  console.log("SYN(WORD): "+ syn(word))
+  console.log("WORD: " + word)
+  console.log("SYNARRAY LENGTH: " + Object.keys(syn).length);
+  var randWord = synArray[Math.floor(Math.random() * Object.keys(syn).length)];
+  console.log("RANDWORD: " + randWord)
+  syn(word)
+}
