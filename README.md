6123bf32b6cca378112ace8942959449b77a5770
Ankit Gupta <ankit0504@gmail.com>
2018-07-17 21:29:23 -0400

    narrows to one synonym

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # etymology-chrome-extension
 Attempting to make a chrome extension that lets a user easily find the etymology of a word
+$ browserify content.js -o bundle.js
--- content.js
+++ content.js
@@ -2,20 +2,30 @@ chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
       var selected = (window.getSelection().toString()).split(" ");
-      for (let word of selected) {
-        get_synonym(word)
+      if (selected) {
+        for (let word of selected) {
+          word = get_synonym(word)
+        }
+        console.log(selected)
+      }
+      else {
+        return
       }
     }
   }
 );
 function get_synonym(word){
   var syn = require("synonyms");
-  var synArray = syn(word);
-  console.log("SYNARRAY: "+ synArray)
-  console.log("SYN(WORD): "+ syn(word))
-  console.log("WORD: " + word)
-  console.log("SYNARRAY LENGTH: " + Object.keys(syn).length);
-  var randWord = synArray[Math.floor(Math.random() * Object.keys(syn).length)];
-  console.log("RANDWORD: " + randWord)
-  syn(word)
+  var withPartOfSpeech = syn(word);
+  var synonyms = randomPartOfSpeech(withPartOfSpeech);
+  var randSynonym = synonyms[Math.floor(Math.random() * synonyms.length)];
+  console.log(synonyms)
+  console.log("*****************")
+  console.log(randSynonym)
+  return randSynonym
 }
+
+function randomPartOfSpeech(object) {
+  var keys = Object.keys(object)
+  return object[keys[ keys.length * Math.random() << 0]];
+};
